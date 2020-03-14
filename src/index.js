a69b979ead1cf5074de1c8acc868c9ddd84db2ce
Abhishek Raj <abhishek@lollypop.design>
2020-03-14 17:29:24 +0530

    add readme

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 
-import 'antd/dist/antd.css';
+import "antd/dist/antd.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
